e59b27239252a55cbbac6aa1bb0ab3640fa1f434
xFarisSx <faristaleek@gmail.com>
2023-03-07 19:44:33 +0300

    trigonometric movements

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const ctx = canvas.getContext('2d')
 
 CANVAS_WIDTH = canvas.width = 700;
 CANVAS_HEIGHT = canvas.height = 760;
-const enemyNumber = 20
+const enemyNumber = 200
 const enemies = []
 
 let dt1 = new Date().getTime()
@@ -14,26 +14,27 @@ let dt = (dt2 - dt1)/1000
 class Enemy {
     constructor() {
         this.image = new Image()
-        this.image.src = "enemy2.png"
+        this.image.src = "enemy3.png"
         this.speed = Math.random() * 400 + 100
-        this.spriteWidth = 266
-        this.spriteHeight = 188
+        this.spriteWidth = 218
+        this.spriteHeight = 177
         this.width = this.spriteWidth/2
         this.height = this.spriteHeight/2
         this.frame = 0
         this.frameSpeed = Math.round(Math.random() * 10 + 5)
         this.x = Math.random() * (canvas.width - this.width)
         this.y = Math.random() * (canvas.height - this.height)
-        this.angle = Math.random() * 2
-        this.angleSpeed = Math.random() * 40
-        this.curve = Math.random() * 7
+        this.angle = Math.random() * 2 
+        this.angleSpeed = Math.random() * 100 + 50
+        this.curve = Math.random() * 200 + 50
     } 
     
     update() {
-        this.x -= this.speed * dt
-        if(this.x + this.width < 0) this.x = canvas.width
-        this.y += this.curve * Math.sin(this.angle) 
+        this.x = canvas.width/2* Math.cos(this.angle * Math.PI/90)+ canvas.width/2- this.width/2
+        this.y =canvas.height/2* Math.sin(this.angle * Math.PI/270)+ canvas.width/2- this.width/2
         this.angle += this.angleSpeed* dt
+        if(this.x + this.width < 0) this.x = canvas.width
+
         this.frame > 4 ? this.frame = 0 : this.frame+=this.frameSpeed * dt
     }
 
