0a35c21c3ddc0e826a12232d6a0dbce12b5d1f3c
xFarisSx <faristaleek@gmail.com>
2023-03-07 20:01:57 +0300

    target based npc smooth movement

--- script.js
+++ script.js
@@ -4,35 +4,44 @@ const ctx = canvas.getContext('2d')
 
 CANVAS_WIDTH = canvas.width = 700;
 CANVAS_HEIGHT = canvas.height = 760;
-const enemyNumber = 200
+const enemyNumber = 20
 const enemies = []
 
 let dt1 = new Date().getTime()
 let dt2 = new Date().getTime()
 let dt = (dt2 - dt1)/1000
 
+let gameFrame = 0
+
 class Enemy {
     constructor() {
         this.image = new Image()
-        this.image.src = "enemy3.png"
+        this.image.src = "enemy4.png"
         this.speed = Math.random() * 400 + 100
-        this.spriteWidth = 218
-        this.spriteHeight = 177
+        this.spriteWidth = 213
+        this.spriteHeight = 213
         this.width = this.spriteWidth/2
         this.height = this.spriteHeight/2
         this.frame = 0
         this.frameSpeed = Math.round(Math.random() * 10 + 5)
         this.x = Math.random() * (canvas.width - this.width)
         this.y = Math.random() * (canvas.height - this.height)
-        this.angle = Math.random() * 2 
-        this.angleSpeed = Math.random() * 100 + 50
-        this.curve = Math.random() * 200 + 50
+
+        this.newX = Math.random() * (canvas.width- this.width)
+        this.newY = Math.random() * (canvas.height- this.height)
+        this.smooth = 0.01
+        this.interval = Math.round(Math.random() * 200 + 50)
     } 
     
     update() {
-        this.x = canvas.width/2* Math.cos(this.angle * Math.PI/90)+ canvas.width/2- this.width/2
-        this.y =canvas.height/2* Math.sin(this.angle * Math.PI/270)+ canvas.width/2- this.width/2
-        this.angle += this.angleSpeed* dt
+        if(gameFrame % this.interval === 0) {
+            this.newX = Math.random() * (canvas.width - this.width)
+            this.newY = Math.random() * (canvas.height - this.height)
+        }
+        let dx = this.x - this.newX
+        let dy = this.y - this.newY
+        this.x -= dx * this.smooth
+        this.y -= dy * this.smooth
         if(this.x + this.width < 0) this.x = canvas.width
 
         this.frame > 4 ? this.frame = 0 : this.frame+=this.frameSpeed * dt
@@ -51,6 +60,7 @@ function animate(){
     dt2 = new Date().getTime()
     dt = (dt2 - dt1)/1000
     dt1 = new Date().getTime()
+    gameFrame++
     ctx.clearRect(0,0, CANVAS_WIDTH, CANVAS_HEIGHT)
     enemies.forEach((en) => {
         en.draw()
