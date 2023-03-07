3f899ef1583884dae7d0983363f19f7ab09a8a3e
xFarisSx <faristaleek@gmail.com>
2023-03-07 19:26:49 +0300

    waves

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const ctx = canvas.getContext('2d')
 
 CANVAS_WIDTH = canvas.width = 700;
 CANVAS_HEIGHT = canvas.height = 760;
-const enemyNumber = 10
+const enemyNumber = 20
 const enemies = []
 
 let dt1 = new Date().getTime()
@@ -14,21 +14,26 @@ let dt = (dt2 - dt1)/1000
 class Enemy {
     constructor() {
         this.image = new Image()
-        this.image.src = "enemy1.png"
-        this.speed = Math.random() * 4 -2
-        this.spriteWidth = 293
-        this.spriteHeight = 155
-        this.width = this.spriteWidth/2.5
-        this.height = this.spriteHeight/2.5
+        this.image.src = "enemy2.png"
+        this.speed = Math.random() * 400 + 100
+        this.spriteWidth = 266
+        this.spriteHeight = 188
+        this.width = this.spriteWidth/2
+        this.height = this.spriteHeight/2
         this.frame = 0
         this.frameSpeed = Math.round(Math.random() * 10 + 5)
         this.x = Math.random() * (canvas.width - this.width)
         this.y = Math.random() * (canvas.height - this.height)
+        this.angle = Math.random() * 2
+        this.angleSpeed = Math.random() * 40
+        this.curve = Math.random() * 7
     } 
     
     update() {
-        this.x += Math.random() * 5 - 2.5
-        this.y += Math.random() * 5 - 2.5
+        this.x -= this.speed * dt
+        if(this.x + this.width < 0) this.x = canvas.width
+        this.y += this.curve * Math.sin(this.angle) 
+        this.angle += this.angleSpeed* dt
         this.frame > 4 ? this.frame = 0 : this.frame+=this.frameSpeed * dt
     }
 
